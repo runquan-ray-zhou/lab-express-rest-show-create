a2af5c24def7b01ea1025337646e6e00e60bb20f
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-27 23:09:50 -0400

    add health check route and catch all error route in app.js

--- app.js
+++ app.js
@@ -4,5 +4,15 @@ const express = require("express");
 // Configuration
 const app = express();
 
+// Health Check Route
+app.get("/", (req, res) => {
+    res.send("Hello, world!")
+})
+
+// 404 Page not found
+app.get("*", (req, res) => {
+    res.status(404).json({ error: "Sorry, no page found" });
+});
+
 // Export
 module.exports = app;
